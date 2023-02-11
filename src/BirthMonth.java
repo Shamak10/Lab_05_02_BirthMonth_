b79db172bb725a92a1a456b5d22e9e3c6007f213
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-11 10:47:59 -0500

    Update BirthMonth.java

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
 public class BirthMonth {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your birth month (1-12): ");// output enter your birth month
-        int birthMonth = (20);
+    public static void main(String[] args) {//enter your birth month
+        int birthMonth = 20;
 
-        if (birthMonth >= 1 && birthMonth <= 12) {// birth month has to between 1 and 12
-            System.out.println("Your birth month is: " + birthMonth);// output you get the correct birth month
+        if (birthMonth >= 1 && birthMonth <= 12) {// Birth month has to be between 1 and 12
+            System.out.println("Your birth month is: " + birthMonth);// output enter your birth month
         } else {
-            System.out.println("You entered an invalid month value: " + birthMonth);//output invalid birth month
+            System.out.println("You entered an incorrect month value: " + birthMonth);//output invalid birth month
         }
     }
 }
